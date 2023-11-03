8beb7ae5abc55d77d4a4419f0663941af783eeb6
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-11-03 20:43:31 +0100

    i fixed the go down and up functionalities and added some animation

---
+++ app/components/WantAWebsite.tsx
@@ -0,0 +1,8 @@
+
+export const WantAWebsite = () => {
+  return (
+    <div className="sticky top-0 bg-primary h-screen text-white flex justify-center items-center">
+      <button className="border-2 border-white py-2 px-4 rounded-full font-bold bg-white text-primary hover:bg-transparent hover:text-white duration-300">I want a 3D website</button>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        primary: "#0f1112"
+      }
+    },
   },
   plugins: [],
 } satisfies Config
